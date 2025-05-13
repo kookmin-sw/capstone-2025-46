a0ef03ab88990543db42f4243aebf71fd711a625
Chungsoo <cjdtn8885@naver.com>
2025-05-13 22:25:10 +0900

    실행파일 생성 명령어

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5 import QtWidgets
 import utils
 from ui.ui_main import MainWindow
 
+# pyinstaller --add-data "C:/Users/cjdtn/PycharmProjects/capstone-2025-46/ui;ui" -F main.py
 
 if __name__ == '__main__':
     utils.init_settings()
